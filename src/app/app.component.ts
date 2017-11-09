1180f9124e4b21cb8a47f24ec6946221307782a9
nfnxmc <mamoudou.n.camara@gmail.com>
2017-11-09 23:43:21 +0100

    FreeCodeCamp Leader Board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  recent  = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
+  alltime = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+  leaders100 = [];
+  leadersall = [];
+  leaders = [];
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(){
+    this.http.get<JSON[]>(this.recent).subscribe( data => {this.leaders = data; this.leaders100 = data;});
+    this.http.get<JSON[]>(this.alltime).subscribe( data => { this.leadersall = data;});
+  }
+
+  settTorecent(){
+  this.leaders = this.leaders100;
+  }
+  setToAlltime(){
+    this.leaders = this.leadersall;
+  }
 }
